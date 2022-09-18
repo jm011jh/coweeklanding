d32bc168e573760509b22d21b908cb24d1ef06c0
jm011jh <75968965+jm011jh@users.noreply.github.com>
2022-09-18 16:10:16 +0900

    123

--- component.js
+++ component.js
@@ -1,2 +1,13 @@
 // $.get("./Header.html",(comp)=>{$("#header").prepend(comp)})
 $.get("./Footer.html",(comp)=>{$("#footer").prepend(comp)})
+
+
+$(window).scroll(function(){
+    var a = window.scrollY
+    if(a>10){
+        $("#header").addClass("color")
+    }else{
+        $("#header").removeClass("color")
+    }
+    
+})
