1fb370890ebfebf1685fb8811326d684de1880b0
jm011jh <75968965+jm011jh@users.noreply.github.com>
2022-09-28 03:27:36 +0900

    asdf

--- component.js
+++ component.js
@@ -1,6 +1,7 @@
 // $.get("./Header.html",(comp)=>{$("#header").prepend(comp)})
 $.get("/Footer.html",(comp)=>{$("#footer").prepend(comp)})
 $.get("/classList.html",(comp)=>{$("#classList").prepend(comp)})
+$.get("/classListMain.html",(comp)=>{$("#classListMain").prepend(comp)})
 
 
 $(window).scroll(function(){
